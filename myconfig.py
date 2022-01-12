3565b15cb154ac3f150a04b3175334c2ff3b28c4
simonboych <simonboych@gmail.com>
2022-01-12 08:04:08 +0000

    喜歡的註解

--- myconfig.py
+++ myconfig.py
@@ -51,8 +51,8 @@
 # 
 # #STEERING
 # STEERING_CHANNEL = 1            #channel on the 9685 pwm board 0-15
-STEERING_LEFT_PWM = 600         #pwm value for full left steering
-STEERING_RIGHT_PWM = 160        #pwm value for full right steering
+STEERING_LEFT_PWM = 580         #pwm value for full left steering
+STEERING_RIGHT_PWM = 220        #pwm value for full right steering
 # 
 # #STEERING FOR PIGPIO_PWM
 # STEERING_PWM_PIN = 13           #Pin numbering according to Broadcom numbers
