82c5fdbc2a9385d92385073e26db709a8d616d44
simonboych <simonboych@gmail.com>
2022-01-14 08:23:15 +0000

    喜歡的註解

--- myconfig.py
+++ myconfig.py
@@ -61,9 +61,9 @@ STEERING_RIGHT_PWM = 220        #pwm value for full right steering
 # 
 # #THROTTLE
 # THROTTLE_CHANNEL = 0            #channel on the 9685 pwm board 0-15
-THROTTLE_FORWARD_PWM = 430      #pwm value for max forward throttle
-THROTTLE_STOPPED_PWM = 320      #pwm value for no movement
-THROTTLE_REVERSE_PWM = 260      #pwm value for max reverse throttle
+THROTTLE_FORWARD_PWM = 380      #pwm value for max forward throttle
+THROTTLE_STOPPED_PWM = 305      #pwm value for no movement
+THROTTLE_REVERSE_PWM = 275      #pwm value for max reverse throttle
 # 
 # #THROTTLE FOR PIGPIO_PWM
 # THROTTLE_PWM_PIN = 18           #Pin numbering according to Broadcom numbers
